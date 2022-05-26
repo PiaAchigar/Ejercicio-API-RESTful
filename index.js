8ff35e431391007f9187c7e0f62e9e9148fc7bc7
Maria <achigarpia@gmail>
2022-05-26 13:27:19 -0300

    index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const productosRouter = require("./productRouter");
 app.use(express.json());
-app.use(express.urlencoded({ express: true }));
+app.use(express.urlencoded({ extended: true }));
 
 const PORT = 8080;
 const server = app.listen(PORT, () => {
